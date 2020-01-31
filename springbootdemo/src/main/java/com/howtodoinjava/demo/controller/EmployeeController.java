6fc9f5dc051353d92c568d3d91ed7dc23019e3c5
sureshreddy6242 <60186161+sureshreddy6242@users.noreply.github.com>
2020-01-31 07:03:56 -0500

    Update EmployeeController.java

--- springbootdemo/src/main/java/com/howtodoinjava/demo/controller/EmployeeController.java
+++ springbootdemo/src/main/java/com/howtodoinjava/demo/controller/EmployeeController.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.howtodoinjava.demo.model.User;
+import eu.rekawek.toxiproxy.Proxy;
+import eu.rekawek.toxiproxy.ToxiproxyClient;
+import eu.rekawek.toxiproxy.model.ToxicDirection;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +27,18 @@ public class EmployeeController {
 		return employeesList;
     }*/
 
+	ToxiproxyClient client;
+	Proxy httpProxy;
+
+	public void setup() throws Exception {
+
+		//toxyproxycliet ip
+		client = new ToxiproxyClient("3.18.108.18", 8474);
+		//client = new ToxiproxyClient("2.3.45.6", 8474);
+
+		// creating toxi proxy for balandeloader
+		httpProxy = client.createProxy("http-tproxy", "3.18.108.18:8888", "3.17.129.12:5000");
+	}
 
 	@GetMapping("/addUser")
 	public String sendForm(User user) {
@@ -39,15 +54,30 @@ public class EmployeeController {
 		return "showMessage";
 	}
 
+//bandwidth
+	@RequestMapping("/bandwidth")
+	public RedirectView addtoxis(User user) throws Exception {
+		setup();
 
-	@RequestMapping("/toxics")
-	public RedirectView addtoxis(User user) {
+		httpProxy.toxics().latency("latency-toxic", ToxicDirection.DOWNSTREAM, 12_000).setJitter(15);
+
 
 		RedirectView view = new RedirectView();
-		view.setUrl("http://localhost:8083/addUser");
+		view.setUrl("http://3.18.108.18:8888/");
 
 		return view;
 	}
+
+//latency
+	@RequestMapping("/latency")
+	public RedirectView addtoxis2(User user) {
+
+		RedirectView view = new RedirectView();
+		view.setUrl("http://3.18.108.18:26379/");
+
+		return view;
+	}
+
 
 
 
